577249e314dcf3a12de02c814ee1686ee0f8ab1a
pericotronic <117849626+pericotronic@users.noreply.github.com>
2023-02-15 23:31:20 +0100

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,11 +1,17 @@
 import pickle
 import pandas as pd
 import numpy as np
+import gzip
 
 model_file = r"Alumno\3-Machine_Learning\Entregas\ML_project\src\model\model_230215232253.pickle.gz"
 
+# Unzip the model file
+with gzip.open(model_file, 'rb') as f_in:
+    with open(r"Alumno\3-Machine_Learning\Entregas\ML_project\src\model\model_230215232253.pickle", 'wb') as f_out:
+        f_out.write(f_in.read())
+
 # Load the model from the file
-with open(model_file, 'rb') as f:
+with open(r"Alumno\3-Machine_Learning\Entregas\ML_project\src\model\model_230215232253.pickle", 'rb') as f:
     model = pickle.load(f)
 
 # Load the test data
